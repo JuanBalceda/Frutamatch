8a97a9975cdab3227f4c7fa5aa049fb1bfc51be2
Juan Balceda <juanbalceda@gmail.com>
2019-01-10 23:28:08 +0100

    CulqiPayment basic activity updated

--- app/src/main/java/com/apps/balceda/fruits/activities/payment/CulqiPaymentActivity.java
+++ app/src/main/java/com/apps/balceda/fruits/activities/payment/CulqiPaymentActivity.java
@@ -1,15 +1,169 @@
 package com.apps.balceda.fruits.activities.payment;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.apps.balceda.fruits.R;
+import com.apps.balceda.fruits.culqi.Card;
+import com.apps.balceda.fruits.culqi.Token;
+import com.apps.balceda.fruits.culqi.TokenCallback;
+import com.apps.balceda.fruits.validation.Validation;
+
+import org.json.JSONObject;
 
 public class CulqiPaymentActivity extends AppCompatActivity {
+
+    Validation validation;
+
+    ProgressDialog progress;
+
+    TextView txtcardnumber, txtcvv, txtmonth, txtyear, txtemail, kind_card, result;
+    Button btnPay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_culqi_payment);
+
+        validation = new Validation();
+
+        progress = new ProgressDialog(this);
+        progress.setMessage("Validando informacion de la tarjeta");
+        progress.setCancelable(false);
+        progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+
+        txtcardnumber = (TextView) findViewById(R.id.txt_cardnumber);
+
+        txtcvv = (TextView) findViewById(R.id.txt_cvv);
+
+        txtmonth = (TextView) findViewById(R.id.txt_month);
+
+        txtyear = (TextView) findViewById(R.id.txt_year);
+
+        txtemail = (TextView) findViewById(R.id.txt_email);
+
+        kind_card = (TextView) findViewById(R.id.kind_card);
+
+        result = (TextView) findViewById(R.id.token_id);
+
+        btnPay = (Button) findViewById(R.id.btn_pay);
+
+        txtcvv.setEnabled(false);
+
+        txtcardnumber.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length() == 0) {
+                    txtcvv.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String text = txtcardnumber.getText().toString();
+                // Validations go here
+                if (s.length() == 0) {
+                    // txtcardnumber.setBackgroundResource(R.drawable.border_error);
+                }
+
+                if (validation.luhn(text)) {
+                    // txtcardnumber.setBackgroundResource(R.drawable.border_sucess);
+                } else {
+                    // txtcardnumber.setBackgroundResource(R.drawable.border_error);
+                }
+
+                int cvv = validation.bin(text, kind_card);
+                if (cvv > 0) {
+                    txtcvv.setFilters(new InputFilter[]{new InputFilter.LengthFilter(cvv)});
+                    txtcvv.setEnabled(true);
+                } else {
+                    txtcvv.setEnabled(false);
+                    txtcvv.setText("");
+                }
+            }
+        });
+
+        txtyear.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String text = txtyear.getText().toString();
+                // Validation goes here
+                if (validation.year(text)) {
+                    // txtyear.setBackgroundResource(R.drawable.border_error);
+                } else {
+                    // txtyear.setBackgroundResource(R.drawable.border_sucess);
+                }
+            }
+        });
+
+        txtmonth.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String text = txtmonth.getText().toString();
+                // Validation goes here
+
+                if (validation.month(text)) {
+                    // txtmonth.setBackgroundResource(R.drawable.border_error);
+                } else {
+                    // txtmonth.setBackgroundResource(R.drawable.border_sucess);
+                }
+            }
+        });
+
+        btnPay.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                progress.show();
+
+                Card card = new Card(txtcardnumber.getText().toString(), txtcvv.getText().toString(), 9, 2020, txtemail.getText().toString());
+
+                Token token = new Token("pk_test_vzMuTHoueOMlgUPj");
+
+                token.createToken(getApplicationContext(), card, new TokenCallback() {
+                    @Override
+                    public void onSuccess(JSONObject token) {
+                        try {
+                            result.setText(token.get("id").toString());
+                        } catch (Exception ex) {
+                            progress.hide();
+                        }
+                        progress.hide();
+                    }
+
+                    @Override
+                    public void onError(Exception error) {
+                        progress.hide();
+                    }
+                });
+
+            }
+        });
+
     }
 }
