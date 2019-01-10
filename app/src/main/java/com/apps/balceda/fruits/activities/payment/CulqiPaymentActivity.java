f0f15bbf59cfccf2e1bb48c433d87581e66225fe
Juan Balceda <juanbalceda@gmail.com>
2019-01-10 23:18:46 +0100

    CulqiPayment empty activity added

---
+++ app/src/main/java/com/apps/balceda/fruits/activities/payment/CulqiPaymentActivity.java
@@ -0,0 +1,15 @@
+package com.apps.balceda.fruits.activities.payment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.apps.balceda.fruits.R;
+
+public class CulqiPaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_culqi_payment);
+    }
+}
